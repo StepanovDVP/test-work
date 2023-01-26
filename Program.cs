1a3ee7a69990cc785c39ad3e38a79f35923ddaf3
Dmitry <stepanov.stal@yandex.ru>
2023-01-26 15:46:12 +0300

    метод EnterArr

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символа.
+
+// ** метод для получение массива ** 
+List<string> EnterArr(string text)
+{
+    Console.WriteLine(text);
+    var ls = new List<string>();
+    Console.WriteLine("Для завершения нажмите (space) или");
+    while (true)
+    {
+        Console.WriteLine("введите значение: ");
+        string st = Console.ReadLine();
+        if (st == " " ) break;
+        else ls.Add(st);
+    }
+    return ls;
+}
+
+var someList = EnterArr("задайте массив");
+Console.WriteLine($"[{string.Join(", ", someList)}]");
